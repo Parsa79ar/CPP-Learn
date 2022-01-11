28c618f4bbd86140bdca3ea3b6daa51d6f270339
Parsa Ardebili <Parsa.ardebili@gmail.com>
2022-01-11 14:07:20 +0330

    Class and Use Constructor function then call from main

--- test.cpp
+++ test.cpp
@@ -1,18 +1,41 @@
 #include <iostream>
+#include <list>
 using namespace std;
 
-void introduceMe(string name)
-{
-    cout << "My name is: " << name << endl;
-}
+class YouTubeChannel {
+public:
+    string Name;
+    string OwnerName;
+    int SubscribersCount;
+    list<string> PublishedVideoTitles;
+
+    YouTubeChannel(string name, string ownerName) {
+        Name = name;
+        OwnerName = ownerName;
+        SubscribersCount = 0;
+    }
+
+    void GetInfo() {
+        cout << "Name: " << Name <<endl;
+        cout << "OwnerName: " << OwnerName <<endl;
+        cout << "SubscribersCount: " << SubscribersCount <<endl;
+        cout << "Videos: " <<endl;
+
+        for (string videoTiltle : PublishedVideoTitles) {
+            cout << videoTiltle <<endl;
+        }
+    }
+};
 
 int main()
 { 
-    string name;
-    cout << "please enter your name: ";
-    cin >> name;
-
-    introduceMe(name);
+    YouTubeChannel ytChannel("Parsa", "Parsa");
+    ytChannel.PublishedVideoTitles.push_back("cpp");
+    ytChannel.PublishedVideoTitles.push_back("php");
+    ytChannel.PublishedVideoTitles.push_back("python");
+    ytChannel.SubscribersCount = 10;
     
+    ytChannel.GetInfo();
+
     return 0;
 }
